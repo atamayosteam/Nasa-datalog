2868398d4b7ccf892698172baa75443c9e6c2815
Vlad Dracula <vlad@tran.sylvan.ia>
2022-12-19 10:45:04 -0500

    graph

---
+++ practice.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+plt.title('graph num 1')
+x = list(range(0, 10))
+y = list(range(-10, 0))
+
+plt.plot(x, y)
+
+plt.show()
+
+plt.title('graph num 2')
+a = [10, 2, 6, -3, 12]
+b = [0, 3, 7, 3, 9]
+plt.plot(a, b)
+plt.show()
+
+plt.title('graph num 3')
+plt.hist(a)
+plt.show()
