cd4ab57b614661bda91718c73ba3f599ced9a5f7
Vlad Dracula <vlad@tran.sylvan.ia>
2023-01-05 10:51:05 -0500

    n

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 import matplotlib.pyplot as plt
 from collections import Counter
+
 file = open('NASA_access_log_Jul95')
 ip_addresses = []
 
 try:
     for line in file:  
         ip_addresses.append(line.split(" ")[0])
+       
 except:
     print("uh-oh")
 
-ip_counter = Counter(ip_addresses)
+ip_counter = Counter(ip_addresses).most_common(10)
+ip_values = []
+ip_keys = []
+
+print(ip_addresses)
 
 c = Counter(ip_addresses)
 
-print(ip_counter.keys())
-print(ip_counter.values())
+newDict = dict()
 
-plt.bar(ip_counter.keys(), ip_counter.values())
+for key, value in c.items():
+    if value >=400: 
+       newDict[key] = value 
+
+
+print(newDict.keys())
+print(newDict.values())
+
+plt.bar(newDict.keys(),newDict.values())
 plt.show()
 
 
